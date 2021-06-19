fec2e74619fe87fae77c2dd6f5bd13b3634f20ae
Shai <smatsri@gmail.com>
2021-06-19 21:08:23 +0300

    added config

--- src/books/data.ts
+++ src/books/data.ts
@@ -1,20 +1,19 @@
-import {ConnectionPool} from 'mssql'
+import { ConnectionPool } from 'mssql'
 import { Book } from "./models"
 
-const pool = new ConnectionPool({
-  user: 'dev',
-  password: '123456',
-  server: 'localhost', 
-  database: 'BooksStore',
-  options:{
-    trustServerCertificate: true
-  }
-}) 
+import config from "config"
 
-export const connect = async ()=> {
+const dbConfig = config.get<any>("Books.db")
+
+console.log(dbConfig);
+
+
+const pool = new ConnectionPool(dbConfig)
+
+export const connect = async () => {
   try {
     await pool.connect()
- 
+
   } catch (error) {
     console.log("failed to connect", error);
   }
@@ -23,4 +22,4 @@ export const connect = async ()=> {
 export const getById = async (id: number): Promise<Book> => {
   const result = await pool.query<Book>`select id,title,releaseDate from [Books] where id = ${id}`
   return result.recordset[0];
-} 
+}
